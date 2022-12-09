ed6cc7b6173af47f503034361e57f788f3d5b2ed
PearlPerfect <perfectpearl2030@gmail.com>
2022-12-09 09:20:47 -0800

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@ include < stdio.h >
  * Description: main - Puts the texts to stdout.
  * Return: 0 should code runs successfully.
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
