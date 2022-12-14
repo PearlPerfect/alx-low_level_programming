624d96f94c3bb2947872deecc7bd7771fc3c4573
PearlPerfect <perfectpearl2030@gmail.com>
2022-12-14 11:40:30 -0800

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- *main - Prints _putchar followed by a new line
- *Return: Always 0.
-*/
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -14,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-Return (0);
+	return (0);
 }
