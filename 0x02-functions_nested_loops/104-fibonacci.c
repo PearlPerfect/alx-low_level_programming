68ba03cc3d9f11bf1ca93e88f74b63ba6c362698
PearlPerfect <perfectpearl2030@gmail.com>
2022-12-14 21:01:50 -0800

    For the love of C

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints 98 fibonacci terms
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int n = 98;
+	int n1 = 1, n2 = 2;
+	int n3 = n1 + n2;
+
+	printf("%d, %d, ", n1, n2);
+	for (i = 3; i < n; ++i)
+	{
+		printf("%d, ", n3);
+		n1 = n2;
+		n2 = n3;
+		n3 = n1 + n2;
+	}
+	printf("\n");
+	return (0);
+}
