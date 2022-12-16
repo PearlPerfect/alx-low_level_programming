907c8dd8aef8e464b4a14919b2d8bb40bbd45fe7
PearlPerfect <perfectpearl2030@gmail.com>
2022-12-16 00:15:17 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - a function that prints the numbers, from 0 to 9
+*
+* Return: void followed by newline
+*/
+
+void print_numbers(void)
+
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num + '0');
+		num += 1;
+	}
+	_putchar('\n');
+}
