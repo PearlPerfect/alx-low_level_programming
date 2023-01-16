1bc73559810925d72c97560f0427def93dfd7d70
PearlPerfect <perfectpearl2030@gmail.com>
2023-01-16 11:34:17 -0800

    Dog life

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,6 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *s);
-void free_dog(dog_t  * d);
+void free_dog(dog_t *d);
 
-#endif`
+#endif
