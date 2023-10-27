25c8da9987fc932b52daeb68a110a9d63d794ef0
PearlPerfect <perfectpearl2030@gmail.com>
2023-10-27 06:38:24 -0700

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 
 	while (next_l)
 	{
-		if (!next_l->str)
-			printf("[0] (nil)");
-		else
+if (!next_l->str)
+printf("[0] (nil)");
+else
 			printf("[%u] %s", next_l->len, next_l->str);
 
 		nodes++;
